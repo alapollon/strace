71075ea4845172a8128ef3583f19bf6da0d374b5
wyvernghost <65455574+alapollon@users.noreply.github.com>
2025-04-25 09:24:06 -0500

    Create fetch _struct_mplg.c
    incremental product sprint

---
+++ src/fetch _struct_mplg.c
@@ -0,0 +1,26 @@
+
+#include <stdlib.h> 
+
+typedef struct n_alloc {
+  struct n_alloc *next;
+  char name[30];
+}
+
+
+struct n_alloc *head;
+extern char name[];
+
+struct n_alloc *insert()
+{
+  struct n_alloc *n_ptr = &head; 
+
+  while (){
+    if()
+      break;
+    
+  }
+  // assignments 
+}
+/* 
+oneway linked list for management of free memory 
+*/ 
