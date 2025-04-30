c58281d7204f65ad431c28dd2857f3b806c55adb
wyvernghost <65455574+alapollon@users.noreply.github.com>
2025-04-30 05:34:38 -0400

    product refinement

---
+++ src/fetch_srtruct_mplg.cc
@@ -0,0 +1,35 @@
+
+#include <stdlib.h> 
+#include <fstream> 
+#include <sstream> 
+#include <cstdio>
+#include <cstdlib>
+
+template <>
+
+typedef struct n_alloc {
+  struct n_alloc *next;
+  char* buffer = new char[255];;
+}
+
+struct n_alloc *head;
+extern char name[];
+
+
+struct n_alloc *insert()
+
+void alloc_poke_data(std::ostream& stream){
+  stream << << 
+}
+
+{
+  struct n_alloc *n_ptr = &head; 
+
+  while (n_alloc->next){
+    if(strcmp())
+      break;
+    
+  }
+}
+/* 
+*/ 
